eeb4d30e827ded07f1650eb947fb796d8ad8f86e
Christophe Préaud <christophe.preaud@kelkoogroup.com>
2021-06-14 10:18:24 +0200

    Add test with WindowSpec.rowsBetween

--- src/test/scala/com/kelkoogroup/dojo/spark/TestSparkWindowAggregationFunctions.scala
+++ src/test/scala/com/kelkoogroup/dojo/spark/TestSparkWindowAggregationFunctions.scala
@@ -22,50 +22,80 @@ class TestSparkWindowAggregationFunctions extends FunSuite with Matchers with Da
   }
 
   lazy val expectedGroupByDF = Seq(
-    (1, "a", 5),
-    (2, "b", 2),
-    (3, "c", 3),
-    (4, "a", 5),
-    (5, "b", 2),
-    (6, "a", 5),
-    (7, "c", 3),
-    (8, "a", 5),
-    (9, "c", 3),
-    (10, "a", 5)
+    ("id01", "a", 5),
+    ("id02", "b", 2),
+    ("id03", "c", 3),
+    ("id04", "a", 5),
+    ("id05", "b", 2),
+    ("id06", "a", 5),
+    ("id07", "c", 3),
+    ("id08", "a", 5),
+    ("id09", "c", 3),
+    ("id10", "a", 5)
   ).toDF("id", "desc", "count")
 
   lazy val df = Seq(
-    (1, "a"),
-    (2, "b"),
-    (3, "c"),
-    (4, "a"),
-    (5, "b"),
-    (6, "a"),
-    (7, "c"),
-    (8, "a"),
-    (9, "c"),
-    (10, "a")
+    ("id01", "a"),
+    ("id02", "b"),
+    ("id03", "c"),
+    ("id04", "a"),
+    ("id05", "b"),
+    ("id06", "a"),
+    ("id07", "c"),
+    ("id08", "a"),
+    ("id09", "c"),
+    ("id10", "a")
   ).toDF("id", "desc")
 
   test("add count column - with groupBy") {
     val dfCountDesc = df.groupBy("desc").count
 
-    val actualGroupByDF = df.join(
-      dfCountDesc, Seq("desc"), "left_outer"
-    ).select("id", "desc", "count")
+    val actualGroupByDF = df
+      .join(
+        dfCountDesc,
+        Seq("desc"),
+        "left_outer"
+      )
+      .select("id", "desc", "count")
 
     actualGroupByDF.collect should contain theSameElementsAs expectedGroupByDF.collect
   }
 
   test("add count column - with Window aggregation function") {
-    val windowIndexRange = Window.partitionBy("desc")
+    val wByDesc = Window.partitionBy("desc")
 
     val actualGroupByDF = df.select(
       $"id",
       $"desc",
-      count("*").over(windowIndexRange).as("count")
+      count("*").over(wByDesc).as("count")
     )
 
     actualGroupByDF.collect should contain theSameElementsAs expectedGroupByDF.collect
+  }
+
+  test("add index column") {
+    val wIndex = Window.orderBy("id")
+
+    val actualDF = df.select(
+      count("*")
+        .over(wIndex.rowsBetween(Window.unboundedPreceding, Window.currentRow))
+        .as("num_row"),
+      $"id",
+      $"desc"
+    )
+
+    val expectedDF = Seq(
+      (1L, "id01", "a"),
+      (2L, "id02", "b"),
+      (3L, "id03", "c"),
+      (4L, "id04", "a"),
+      (5L, "id05", "b"),
+      (6L, "id06", "a"),
+      (7L, "id07", "c"),
+      (8L, "id08", "a"),
+      (9L, "id09", "c"),
+      (10L, "id10", "a")
+    ).toDF("num_row", "id", "desc")
+    actualDF.collect should contain theSameElementsAs expectedDF.collect
   }
 }
